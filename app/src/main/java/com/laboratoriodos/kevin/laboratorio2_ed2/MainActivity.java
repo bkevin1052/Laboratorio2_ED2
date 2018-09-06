7eebc3b200147869823814c95886620bda386d30
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-09-06 08:14:01 -0600

    Agregando activities

--- app/src/main/java/com/laboratoriodos/kevin/laboratorio2_ed2/MainActivity.java
+++ app/src/main/java/com/laboratoriodos/kevin/laboratorio2_ed2/MainActivity.java
@@ -1,13 +1,31 @@
 package com.laboratoriodos.kevin.laboratorio2_ed2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnHuffman;
+    Button btnLZW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHuffman = (Button)findViewById(R.id.btnHuffman);
+        btnLZW = (Button)findViewById(R.id.btnLzw);
+
+        btnHuffman.setOnClickListener(view ->{
+            startActivity(new Intent(getApplicationContext(), FilesActivity.class));
+        });
+
+        btnLZW.setOnClickListener(view ->{
+            startActivity(new Intent(getApplicationContext(),FilesActivity.class));
+        });
     }
 }
